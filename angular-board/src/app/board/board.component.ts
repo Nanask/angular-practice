e42a34ff3746d45af3a99dab917b58d9e1ab6814
Nanask <ybhag1005@daum.net>
2022-04-25 23:43:18 +0900

    리스트 보여주기

--- angular-board/src/app/board/board.component.ts
+++ angular-board/src/app/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IBoardDTO } from 'src/interface/dto/board.dto';
+import { BoardService } from './../../api/board/board.service';
 
 @Component({
   selector: 'app-board',
@@ -6,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  boardlist: IBoardDTO[];
 
-  constructor() { }
+  constructor(private boardService: BoardService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.boardlist = [];
+    this.boardService.findAll().subscribe((res) => {
+      if (res) {
+        const _boardlist = res;
 
+        this.boardlist = _boardlist.filter((board) => {
+          return board?.writer === localStorage.getItem('nickname');
+        });
+      }
+    });
+  }
 }
