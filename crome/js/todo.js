f656cbc9243935333b1c50d081ec826407d3e892
Nanask <ybhag1005@daum.net>
2022-06-08 00:33:02 +0900

    todo 코드 수정

--- crome/js/todo.js
+++ crome/js/todo.js
@@ -1,47 +1,35 @@
 const todo = document.querySelector(".todo_form");
 const todoItem = document.querySelector(".todo_form input");
 const todoList = document.querySelector(".todo_list");
-console.log("todo", todo);
-console.log("todo", todoList);
 
 const TODOS = "todos";
 
 let todos = [];
 let id = 0;
 
-function todoDelete(e) {
-  console.log("event", e.target.parentElement);
-  const li = e.target.parentElement;
-  li.remove();
+function saveTodo() {
+  localStorage.setItem(TODOS, JSON.stringify(todos));
 }
 
-function todoUpdate(e, liId) {
+function todoDelete(todoId) {
+  const _todo = todos.filter((todo) => {
+    return todo.id !== todoId;
+  });
+  todos = _todo;
+  paintTodos(_todo);
+  saveTodo();
+}
+
+function todoUpdate(e, todoId) {
   if (e.keyCode === 13) {
     const value = e.target.value;
-    console.log("value", value);
-    // todos.map((todo) => {
-    //   console.log("todo", todo);
-    //   console.log("todo", todo.id);
-    //   todo.id === liId ? { ...todo, content: value } : todo;
-    // });
-
-    // console.log("set", set);
-
-    // for (let i = 0; todos.length; i++) {
-    // for (let todo in todos) {
-    //   // console.log("todos", todos[todo]);
-    //   // console.log("todo", todo);
-    //   // console.log("liId", liId);
-    //   todo === liId ? todos.splice(todo, 1, { content: value, id: todo }) : todo;
-    //   // todo === liId ? todos.splice(todo, 1, { content: value, id: todo }) : todo;
-    // }
-
-    todos.forEach((todo) => {
-      console.log("todos", todo);
+    const _todos = todos.map((todo) => {
+      return todo.id == todoId ? { ...todo, content: value } : todo;
     });
-    console.log("todos", todos);
+    todos = _todos;
+    paintTodos(_todos);
+    saveTodo();
   }
-  // console.log("todos", todos);
 }
 
 function todoDblclick(e) {
@@ -52,48 +40,95 @@ function todoDblclick(e) {
   li.appendChild(input);
   const inputText = text.innerText;
   input.setAttribute("value", inputText);
-  // console.log("li", li);
-  // console.log("li", li.id);
-
-  li.addEventListener("keyup", (e) => todoUpdate(e, li.id));
+  const todoId = li.id;
+  li.addEventListener("keyup", (e) => todoUpdate(e, todoId));
 }
 
-function todoAdd(todoValue) {
-  const li = document.createElement("li");
-  const span = document.createElement("span");
-  const button = document.createElement("button");
-  span.innerText = todoValue;
-  button.innerHTML = "X";
-  button.addEventListener("click", todoDelete);
-  li.appendChild(span);
-  todoList.appendChild(li);
-  li.appendChild(button);
-  todos.map((todo) => {
+function todoCompleted(e, todoId) {
+  const checkBox = e.target.parentElement;
+  // const check = checkBox.childNodes;
+  const check = document.getElementById("check");
+  // check.style.color = "red";
+
+  console.log("check", check);
+
+  // // const _todos = todos.map((todo) => {
+  //   console.log("todoId", todoId);
+  // console.log("todo.id", todo.id);
+  // return todo.id == todoId ? { ...todo, completed: true } : { ...todo, completed: false };
+  if (todo.id == todoId) {
+    todos = { ...todos, completed: todo.completed };
+    check.style.color = "red";
+  } else if (!todo.completed) {
+    todos = { ...todo, completed: todo.completed };
+    check.style.color = "white";
+  }
+  // return todos;
+  // });
+
+  console.log("_todos", todos);
+}
+
+// Create 한 요소 그려주기
+function paintTodos() {
+  todoList.innerHTML = null;
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.classList.add("todo_item");
+
+    const span = document.createElement("span");
+
+    const button = document.createElement("button");
+    button.classList.add("todo_delete");
+
+    const check = document.createElement("div");
+    check.classList.add("check_box");
+
+    // HTML 추가
+    span.innerText = todo.content;
+    button.innerHTML = '<i class="fa-solid fa-x"></i>';
+    check.innerHTML = '<i class="fa-solid fa-check" id="check"></i>';
+
+    //Delete
+    button.addEventListener("click", () => todoDelete(todo.id));
+
+    //Completed
+    check.addEventListener("click", (e) => todoCompleted(e, todo.id));
+
+    todoList.appendChild(li);
+    li.appendChild(check);
+    li.appendChild(span);
+    li.appendChild(button);
     li.setAttribute("id", todo.id);
+    span.addEventListener("dblclick", (e) => todoDblclick(e));
   });
-  span.addEventListener("dblclick", (e) => todoDblclick(e));
 }
 
+// Create
 function todoSubmit(e) {
   const number = id++;
   e.preventDefault();
   const todoValue = todoItem.value;
   todoItem.value = "";
-  todos.push({ content: todoValue, id: number });
-  todoAdd(todoValue);
-  // saveTodo();
+  const todoObj = { content: todoValue, id: number, completed: false };
+  // todos.push({ content: todoValue, id: number });
+  todos.push(todoObj);
+  console.log("todosObj", todoObj);
+  paintTodos();
+  saveTodo();
 }
 
 todo.addEventListener("submit", todoSubmit);
 
-// function saveTodo() {
-//   localStorage.setItem(TODOS, JSON.stringify(todos));
-// }
+const savedTodo = localStorage.getItem(TODOS);
+console.log("saved", savedTodo);
 
-// const savedTodo = localStorage.getItem(TODOS);
-// console.log("saved", savedTodo);
+if (savedTodo !== null) {
+  const parsedTodo = JSON.parse(savedTodo);
+  // console.log("parsedTodo", parsedTodo());
+  todos = parsedTodo;
+  console.log("todos", todos);
+  parsedTodo.forEach(paintTodos);
 
-// if (savedTodo !== null) {
-//   const parsedTodo = JSON.parse(savedTodo);
-//   console.log(parsedTodo);
-// }
+  // console.log("todos", todos);
+}
